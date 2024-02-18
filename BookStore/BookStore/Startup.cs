1d3d9a143a106b40550bde58044b63378563dec9
Talib Siddiquee <87259972+Talib-Siddiquee@users.noreply.github.com>
2024-02-18 16:03:12 +0530

    Created book store application

---
+++ BookStore/BookStore/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace BookStore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(async endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
